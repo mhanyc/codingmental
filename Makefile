bfb7604f5ab0771f732d63b50b4e35264ea01ee5
Anders Pearson <anders@columbia.edu>
2016-04-07 13:45:10 +0100

    lunr.js based search
    (still not styled)

--- Makefile
+++ Makefile
@@ -4,10 +4,9 @@ STAGING_BUCKET=compiled.stage.ccnmtl.columbia.edu
 PROD_BUCKET=compiled.ccnmtl.columbia.edu
 INTERMEDIATE_STEPS ?= make $(PUBLIC)/js/all.json
 
-#JS_FILES=static/js/search.js static/js/srcswap.js static/js/alphalist.js static/js/bgswap.js \
-static/js/scrollshrink.js static/js/scrollspy.js static/js/widgets.js
+JS_FILES=static/js/src
 
-#all: jshint jscs
+all: jshint jscs
 
 include *.mk
 
---
+++ static/js/src/search.js
@@ -0,0 +1,66 @@
+var index = lunr(function() {
+    this.field('url');
+    this.field('title', {boost: 10});
+    this.field('author', {boost: 5});
+    this.field('tags', {boost: 5});
+    this.field('content');
+});
+index.ref('url');
+var data = {};
+$.getJSON('/js/all.js').done(function(item) {
+    item.forEach(function(d) {
+        index.add(d);
+        data[d.url] = d;
+    });
+}).fail(function(jqxhr, textStatus, error) {
+    var err = textStatus + ', ' + error;
+    console.error('Error getting Hugo index file:', err);
+});
+
+var doSearch = function() {
+    var q = $('#q').val();
+    var results = index.search(q);
+    var $el = $('#search-results');
+    $el.empty();
+    $el.show();
+    $el.append('<div class="arrow"></div>');
+    $el.append(
+       $('<h2>RESULTS FOR: "' + q + '"</h2>')
+    );
+    if (results.length === 0) {
+        $el.append('<div class="q-no-item">Unfortunately, there are ' +
+                   'no results matching what you\'re looking for.');
+    } else {
+        for (var r in results.slice(0, 10)) {
+            if (results.hasOwnProperty(r)) {
+                var d = data[results[r].ref];
+                var $result = $('<div class="q-item">');
+                $result.append($('<a>', {
+                    href: d.url,
+                    text: d.title
+                }));
+                $el.append($result);
+            }
+        }
+    }
+    return false;
+};
+
+var clearSearch = function() {
+    $('#search-results').empty();
+    $('#search-results').hide();
+};
+
+$(document).ready(function() {
+    $('#search').click(doSearch);
+    $('#clear-search').click(clearSearch);
+    $('#q').keyup(function() {
+        $('#search-results').empty();
+        if ($(this).val().length < 2) {
+            $('#search-results').hide();
+            return;
+        }
+        return doSearch();
+    });
+
+});
